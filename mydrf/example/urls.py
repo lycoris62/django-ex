7867313558367bf13de3d6d816d2493a3181c7c0
barami62 <wodbs7758@naver.com>
2024-05-17 20:19:30 +0900

    Mixins 사용

--- mydrf/example/urls.py
+++ mydrf/example/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI
+from .views import HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI, BooksAPIMixins, BookAPIMixins
 
 urlpatterns = [
   path("hello/", HelloAPI),
   path("fbv/books/", booksAPI),
   path("fbv/book/<int:bid>/", bookAPI),
   path("cbv/books/", BooksAPI.as_view()),
-  path("cbv/book/<int:bid>", BookAPI.as_view())
+  path("cbv/book/<int:bid>", BookAPI.as_view()),
+  path("mixin/books/", BooksAPIMixins.as_view()),
+  path("mixin/book/<int:bid>", BookAPIMixins.as_view()),
+
 ]
--- mydrf/example/views.py
+++ mydrf/example/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, viewsets, permissions, generics
+from rest_framework import status, viewsets, permissions, generics, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 
@@ -51,3 +51,25 @@ class BookAPI(APIView):
     book = get_object_or_404(Book, bid=bid)
     serializer = BookSerializer(book)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
+  def get(self, request, *args, **kwargs):
+    return self.list(request, *args, **kwargs)
+  def post(self, request, *args, **kwargs):
+    return self.create(request, *args, **kwargs)
+
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  lookup_field = 'bid' # 기본 모델 pk 가 아닌, bid 를 pk 로 사용하고 있으니 lookup_field 로 사용
+
+  def get(self, request, *args, **kwargs):
+    return self.retrieve(request, *args, **kwargs)
+  def put(self, request, *args, **kwargs):
+    return self.update(request, *args, **kwargs)
+  def delete(self, request, *args, **kwargs):
+    return self.destroy(request, *args, **kwargs)
