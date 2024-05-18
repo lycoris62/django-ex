1976b680f7341a0f35d6d701009061b7420ed0f5
barami62 <wodbs7758@naver.com>
2024-05-18 13:11:51 +0900

    뷰 및 시리얼라이저 추가

--- myboard/users/serializers.py
+++ myboard/users/serializers.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password # 장고 기본 비밀번호 검증
+from django.contrib.auth.password_validation import validate_password  # 장고 기본 비밀번호 검증
 from django.contrib.auth import authenticate
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from rest_framework.validators import UniqueValidator # 이메일 중복 검증
+from rest_framework.validators import UniqueValidator  # 이메일 중복 검증
+
+from .models import Profile
 
 
 class RegisterSerializer(serializers.ModelSerializer):
   email = serializers.EmailField(
       required=True,
-      validators = [UniqueValidator(queryset=User.objects.all())]
+      validators=[UniqueValidator(queryset=User.objects.all())]
   )
   password = serializers.CharField(
       write_only=True,
@@ -41,9 +43,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     token = Token.objects.create(user=user)
     return user
 
-class LoginSerializer(serializers.Serializer): # 모델과 관련 없음, 사용자의 id,pw 받아서 그에 대한 토큰만 줌
+
+class LoginSerializer(serializers.Serializer):  # 모델과 관련 없음, 사용자의 id,pw 받아서 그에 대한 토큰만 줌
   username = serializers.CharField(required=True)
   password = serializers.CharField(required=True, write_only=True)
+
   # write_only 옵션을 통해 클라->서버 방향의 역직렬화는 가능, 서버->클라는 불가능
 
   def validate(self, data):
@@ -54,3 +58,9 @@ class LoginSerializer(serializers.Serializer): # 모델과 관련 없음, 사용
     raise serializers.ValidationError({
       "error": "Unable to login with provided credentials."
     })
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Profile
+    fields = ('nickname', 'position', 'subjects', 'image')
